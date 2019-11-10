069ecdd216dbca72d6576f815374b0b23f7aa80b
aleksa0809 <5457678@mail.ru>
2019-11-10 21:18:49 +0300

    new

---
+++ string_list1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace string_list
+{
+    class MainClass
+    {
+        static List<string> list = new List<string>();
+
+        public static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter pets name: ");
+                string name = Console.ReadLine();
+                list.Add(name);
+
+            }
+            foreach (string name in list)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
+
